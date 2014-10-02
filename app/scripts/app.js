2b9bcc7f85a626c10b388f50b615b401b6a8300b
vagrant <vagrant@precise32.(none)>
2014-10-02 17:47:03 +0000

    Converted album view to Angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,6 +33,12 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
      controller: 'Landing.controller',
      templateUrl: '/templates/landing.html'
    });
+
+   $stateProvider.state('album', {
+     url: '/album',
+     templateUrl: '/templates/album.html',
+     controller: 'Album.controller'
+   });
  }]);
 
  blocJams.controller('Landing.controller', ['$scope', function($scope) {
@@ -67,4 +73,35 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
     }]);
 
  
+ blocJams.controller('Album.controller', ['$scope', function($scope) {
+   $scope.album = angular.copy(albumPicasso);
 
+   var hoveredSong = null;
+   var playingSong = null;
+ 
+   $scope.onHoverSong = function(song) {
+     hoveredSong = song;
+   };
+ 
+   $scope.offHoverSong = function(song) {
+     hoveredSong = null;
+   };
+
+   $scope.getSongState = function(song) {
+     if (song === playingSong) {
+       return 'playing';
+     }
+     else if (song === hoveredSong) {
+       return 'hovered';
+     }
+     return 'default';
+
+     $scope.playSong = function(song) {
+      playingSong = song;
+    };
+ 
+    $scope.pauseSong = function(song) {
+      playingSong = null;
+    };
+   };
+ }]);
